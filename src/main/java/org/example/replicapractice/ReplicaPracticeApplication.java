4376615bc6f3c616f627d58863ba37a792ee13d0
kimseokjun <k03s13j@naver.com>
2025-10-24 12:08:51 +0900

    init

---
+++ src/main/java/org/example/replicapractice/ReplicaPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.replicapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicaPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/replicapractice/ReplicaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.replicapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
